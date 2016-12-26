8d3b5eeda28a81a80233fd610f877b52cd420efb
RamyaKeerthana <keerthana.a@students.iiit.ac.in>
2016-12-26 17:20:00 +0530

    Customer Activity Web Service

---
+++ Invoice/src/com/service/invoice/Customer_Activity.java
@@ -0,0 +1,21 @@
+package com.service.invoice;
+
+public class Customer_Activity {
+   private Integer days;
+   private String emailid;
+   
+
+   public void setDays(Integer days) {
+      this.days = days;
+   }
+   public Integer getDays() {
+      return days;
+   }
+
+   public void setEmailid(String emailid) {
+      this.emailid = emailid;
+   }
+   public String getEmailid() {
+      return emailid;
+   }
+}
---
+++ Invoice/src/com/service/invoice/Customer_Activity_Controller.java
@@ -0,0 +1,25 @@
+package com.service.invoice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class Customer_Activity_Controller {
+
+   @RequestMapping(value = "/customeractivity", method = RequestMethod.GET)
+   public ModelAndView student() {
+      return new ModelAndView("customeractivity", "command", new Customer_Activity());
+   }
+   
+   @RequestMapping(value = "/addCustomeractivity", method = RequestMethod.POST)
+   public String addCustomerinput(@ModelAttribute("SpringWeb")Customer_Activity customer, 
+   ModelMap model) {
+      model.addAttribute("emailid", customer.getEmailid());
+      model.addAttribute("days", customer.getDays());
+      return "result";
+   }
+}
